7e2d0781590479244cce6ef4c66f1f565c6a8295
roy astronomia <roy.astronomia@gmail.com>
2021-12-28 12:27:43 +1300

    Add navigation and main content

--- src/app/contactmanager/components/main-content/main-content.component.ts
+++ src/app/contactmanager/components/main-content/main-content.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-main-content',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainContentComponent implements OnInit {
 
-  constructor() { }
+  user?: User | null;
+
+  constructor(private route: ActivatedRoute, private userService: UserService) {
+
+  }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      let id = params['id'];
+      if(!id) id = 1;
+      this.user = null;
+
+      this.userService.users.subscribe(users => {
+        if (users.length == 0) return;
+
+        setTimeout(() => {
+          this.user = this.userService.userById(id);
+        }, 500);        
+      })
+
+    })
   }
 
 }
